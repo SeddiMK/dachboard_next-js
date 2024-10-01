55e167b6dcdae532c3f264fabbfd7fdbda177c8a
SeddiMK <web.egorovm@gmail.com>
2024-10-01 12:37:28 +0300

    header_tailw

--- src/app/pages/myInfo/page.tsx
+++ src/app/pages/myInfo/page.tsx
@@ -9,14 +9,15 @@ import {
 // <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
 
 export default function TimeOff() {
+	// relative top-[-14px] left-[-57px]  //relative top-[80px] left-1/2 transform -translate-x-1/2
 	return (
-		<section className=' w-full flex  justify-center items-start'>
-			<div className='header-main relative top-[-176px] z-50  h-44 flex items-end justify-center bg-slate-200/60 relative top-[80px] left-1/2 transform -translate-x-1/2 '>
+		<section className='relative top-[-176px] w-full flex flex-col justify-center items-center'>
+			<div className='header-main  h-44 flex items-end justify-center bg-slate-200/60  '>
 				<HeaderMainMyInfo />
 			</div>
 
 			<div className='container-body max-w-[70rem] w-full flex justify-between items-start '>
-				<aside className='aside-my-info text-sm font-medium relative top-[-14px] left-[-57px] '>
+				<aside className='aside-my-info text-sm font-medium  '>
 					<AsideContacts />
 					<AsideDate />
 					<AsidePosition />
