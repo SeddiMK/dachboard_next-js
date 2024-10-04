5cb3ab1dacaa8e24e2a151705cef287d12ba039d
SeddiMK <web.egorovm@gmail.com>
2024-10-04 14:50:54 +0300

    clear

--- src/app/pages/myInfo/page.tsx
+++ src/app/pages/myInfo/page.tsx
@@ -1,18 +1,8 @@
-import HeaderMain from '@/components/header-main'
 import HeaderMainMyInfo from '@/components/header-main-my-info'
-import {
-	AsideContacts,
-	AsideDate,
-	AsidePosition,
-	AsideReportsDirect,
-} from '@/components/ui/aside'
-import { Link } from 'lucide-react'
 import TimeOff from '@/components/time-off'
-
-// <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+import AsideMyInfo from '@/components/aside-my-info'
 
 export default function TimeOffPage() {
-	// relative top-[-14px] left-[-57px]  //relative top-[80px] left-1/2 transform -translate-x-1/2
 	return (
 		<section className='time-off-page px-[4.4rem] relative top-[-176px] w-full flex flex-col justify-center items-center'>
 			{/* <div className='header-main relative z-10  h-44 flex items-end justify-center bg-slate-200/60  '>
